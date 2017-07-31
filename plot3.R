f2887b1ac201a7572a7c057149104d1a580d572e
sanjeeban57 <sanjeeban57@gmail.com>
2017-07-31 06:07:37 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,15 @@
+Data1<-read.table("C:/Users/USER/Desktop/HCL/household_power_consumption.txt",sep=";",dec=".",header = TRUE)
+Date<-as.Date(Data1$Date,format="%d/%m/%Y")
+subsetdata<-subset(Data1,Data1$Date=="1/2/2007"|Data1$Date=="2/2/2007")
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+------FOR PLOTTING PNG----------
+dev.copy(png,'plot3.png', width=480, height=480)
+dev.off()
